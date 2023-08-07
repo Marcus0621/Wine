e6417d1d60c34014538c9e23ad40537a4f2a671f
Marcus0621 <141002604+Marcus0621@users.noreply.github.com>
2023-08-07 14:34:08 +0800

    s

--- Pages/🍷TYPES OF WINE.py
+++ Pages/🍷TYPES OF WINE.py
@@ -1,15 +1,5 @@
 import streamlit as st
 
-# Add custom CSS to style the buttons
-st.markdown("""
-<style>
-.sidebar .widget>div>div>div>button {
-    width: 100%; /* Set a fixed width for the buttons */
-    padding: 8px; /* Add padding for better appearance */
-}
-</style>
-""", unsafe_allow_html=True)
-
 button1 = st.sidebar.button("RED WINE")
 button2 = st.sidebar.button("WHITE WINE")
 button3 = st.sidebar.button("ROSE WINE")
@@ -17,7 +7,9 @@ button4 = st.sidebar.button("AMBER WINE")
 button5 = st.sidebar.button("DESSERT WINE")
 button6 = st.sidebar.button("SPARKLING WINE")
 
+
 if any([button1, button2, button3, button4, button5, button6]):
+
     if button1:
         st.title("Red Wine Information")
     elif button2:
