feb15c2ed9562a66951a695834355d62627f70e6
Marcus0621 <141002604+Marcus0621@users.noreply.github.com>
2023-08-07 15:23:34 +0800

    s

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 # main.py
 
 import streamlit as st
-from TYPES_OF_WINE import  display_wine_types
+from TYPES_OF_WINE import display_wine_types
 from HOMEPAGE import display_homepage
 
 # Create a sidebar navigation option
 selected_page = st.sidebar.radio("Select Page", ("Home", "Types of Wine"))
 
 # Run the appropriate Streamlit script based on the selected page
-if selected_page == "Home page":
+if selected_page == "Home":
     display_homepage()
-elif selected_page == "Types Of Wine":
+elif selected_page == "Types of Wine":
     display_wine_types()
