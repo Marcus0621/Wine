9c0c2a982e2de4e4d99960cbdb20eb1fe2f27077
Marcus0621 <141002604+Marcus0621@users.noreply.github.com>
2023-08-07 15:17:32 +0800

    s

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from HOMEPAGE import display_homepage
 selected_page = st.sidebar.radio("Select Page", ("Home", "Types of Wine"))
 
 # Run the appropriate Streamlit script based on the selected page
-if selected_page == "📚HOMEPAGE":
+if selected_page == "HOMEPAGE":
     display_homepage()
-elif selected_page == "🍷TYPES OF WINE":
+elif selected_page == "TYPES OF WINE":
     display_wine_types()
