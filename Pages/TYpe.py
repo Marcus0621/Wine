81be134c82a535933a215f999d30d4494a42c7b1
Marcus0621 <141002604+Marcus0621@users.noreply.github.com>
2023-08-07 14:14:58 +0800

    d

--- Pages/TYpe.py
+++ Pages/TYpe.py
@@ -1,2 +1,27 @@
 import streamlit as st
 
+button1 = st.sidebar.button("RED WINE")
+button2 = st.sidebar.button("WHITE WINE")
+button3 = st.sidebar.button("ROSE WINE")
+button4 = st.sidebar.button("AMBER WINE")
+button5 = st.sidebar.button("DESSERT WINE")
+button6 = st.sidebar.button("SPARKLING WINE")
+
+
+if any([button1, button2, button3, button4, button5, button6]):
+    # welcome_title.empty()
+    # image_placeholder.empty()
+    # caption_placeholder.empty()
+
+    if button1:
+        st.title("Red Wine Information")
+    elif button2:
+        st.title("White Wine Information")
+    elif button3:
+        st.title("Rosé Wine Information")
+    elif button4:
+        st.title("Amber Wine Information")
+    elif button5:
+        st.title("Dessert Wine Information")
+    elif button6:
+        st.title("Sparkling Wine Information")
