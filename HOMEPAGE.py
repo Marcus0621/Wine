88fbac750121c5fc7827e959de8e690ed29ae777
Marcus0621 <141002604+Marcus0621@users.noreply.github.com>
2023-08-07 15:09:09 +0800

    D

---
+++ HOMEPAGE.py
@@ -0,0 +1,11 @@
+# HOMEPAGE.py
+
+import streamlit as st
+from PIL import Image
+
+def display_homepage():
+    welcome_title = st.title("WELCOME TO THE WORLD OF WINE!")
+    first_image = Image.open('WineIntro.jpg')
+    image_placeholder = st.empty()
+    caption_placeholder = st.empty()
+    image_placeholder.image(first_image, caption='Image by 1Zoom', use_column_width=True)
---
+++ Pages/main.py
@@ -0,0 +1,14 @@
+# main.py
+
+import streamlit as st
+from TYPES_OF_WINE import display_wine_types
+from HOMEPAGE import display_homepage
+
+# Create a sidebar navigation option
+selected_page = st.sidebar.radio("Select Page", ("Home", "Types of Wine"))
+
+# Run the appropriate Streamlit script based on the selected page
+if selected_page == "Home":
+    display_homepage()
+elif selected_page == "Types of Wine":
+    display_wine_types()
